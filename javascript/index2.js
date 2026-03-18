09d76669c9d9f402dffb390b805c4ad404fc2830
“Anna” <cuuanna@gmail.com>
2026-03-18 09:50:57 +0100

    js added

---
+++ javascript/index2.js
@@ -0,0 +1,5 @@
+let myName = "Anna";
+let age = 24;
+let favoriteColor = "green";
+
+console.log("Hello! My name is " + myName + ", I am " + age + " years old and my favorite color is " + favoriteColor + ".");
