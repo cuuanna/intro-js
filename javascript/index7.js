6de87094d268e5196b0b57556ebcd97104ec2e5d
“Anna” <cuuanna@gmail.com>
2026-04-14 11:25:23 +0200

    lesson 5, objects and scopes starting code added

---
+++ javascript/index7.js
@@ -0,0 +1,15 @@
+function atm() {
+  const message = parseFloat (
+    prompt (
+	  "Select a choice 1.) See balance 2.) Make a deposit 3.) Make a withdraw 4.) Get account name 5.) Exit"
+	)
+  );
+
+  switch (message) {
+    case 1:
+    case 2:
+    case 3:
+    case 4: 
+    case 5:
+  }
+} 
